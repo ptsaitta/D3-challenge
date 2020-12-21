17fbbdc7c9762d5c260692ade58dd89b7b181efc
Patrick Saitta <ptsaitta@gmail.com>
2020-12-20 19:26:35 -0500

    append svg to scatter div

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,31 @@
 // @TODO: YOUR CODE HERE!
+
+//This is where we are to append our code: <div id="scatter">
+
+//define width
+var width = parseInt(d3.select("#scatter").style("width"));
+
+//define height
+var height = width - width / 3.8;
+
+//define spacing for label on graph
+var labelArea = 100;
+
+//define margin for graph
+var margin = 20;
+
+
+
+
+
+var svg = d3.select("#scatter").append("svg");
+
+function updateRadius() {
+    if (width <= 500) {
+        circleRadius = 5;
+    }
+    else {
+        circleRadius = 10;
+    }
+}
+updateRadius()
